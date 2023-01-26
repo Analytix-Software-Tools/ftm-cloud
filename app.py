8e9c44a4b085735f20375d87c5085742c7b5af0f
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2023-01-26 15:31:14 -0500

    Reports

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from domains.categories.controllers.controller import categories_router
 from domains.attributes.controllers.controller import router as attribute_router
 from domains.product_types.controllers.controller import product_type_router
 from domains.products.controllers.controller import product_router
+from domains.reports.controllers.controller import router as reports_router
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRoute
 
@@ -61,4 +62,6 @@ app.include_router(attribute_router, tags=['Attributes'], prefix='/api/v0/attrib
 app.include_router(product_type_router, tags=['Product Types'], prefix='/api/v0/product_types',
                    dependencies=[Depends(token_listener)])
 app.include_router(product_router, tags=['Products'], prefix='/api/v0/products',
+                   dependencies=[Depends(token_listener)])
+app.include_router(reports_router, tags=['Reports'], prefix='/api/v0/search',
                    dependencies=[Depends(token_listener)])
---
+++ models/reports.py
@@ -0,0 +1,83 @@
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
+from pydantic.schema import Generic
+
+from models.attribute import AttributeValue
+from models.response import DataT
+
+
+class ProductSearchQuery(BaseModel):
+    """
+    Represents a request model that allows the user to specify a query.
+    """
+    searchText: str
+    productTypePid: str
+    requirements: list[AttributeValue]
+    limit: int = 10
+
+    class Config:
+        scheme_extra = {
+            "example": {
+                "searchText": "Search Text",
+                "productTypePid": "productTypePid",
+                "limit": 10,
+                "requirements": [
+                    {
+                        "attributePid": "attributePid",
+                        "value": {
+                            "numValue": 4
+                        }
+                    }
+                ]
+            }
+        }
+
+
+class Hit(GenericModel, Generic[DataT]):
+    """Represents a ranked result as designated in a HitList. Scoring varies
+    based on the search algorithm that is applied.
+    """
+
+    hit: DataT
+    score: float
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "hit": {
+                    "name": "Product Name",
+                    "description": "Product Description",
+                    "organizationPid": "Organization Pid",
+                    "attributeValues": [],
+                },
+                "score": -1.1
+            }
+        }
+
+
+class HitList(GenericModel, Generic[DataT]):
+    """Hitlists are the results of search endpoints. Generally, results in the hitlist
+    are ranked based on relevance.
+    """
+    hits: list[Hit[DataT]] = []
+    total: int = 0
+    maxScore: float = 0.0
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "hits": [
+                    {
+                        "hit": {
+                            "name": "Product Name",
+                            "description": "Product Description",
+                            "organizationPid": "Organization Pid",
+                            "attributeValues": [],
+                        },
+                        "score": -1.1
+                    }
+                ],
+                "total": 1,
+                "maxScore": -1.1
+            }
+        }
